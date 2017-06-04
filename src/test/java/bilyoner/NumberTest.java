56ee4bbcbd0e4b7a47e71034298e679ee0d7a892
Huseyin Kilic <huseyinjkilic@gmail.com>
2017-06-04 08:41:54 +0300

    Added test cases..

--- src/test/java/bilyoner/NumberTest.java
+++ src/test/java/bilyoner/NumberTest.java
@@ -1,22 +1,25 @@
 package bilyoner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.junit.After;
+
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NumberTest {
 
-	private MockMvc mockMvc;
 	
     @Autowired
     NumberRepository repository;
@@ -38,45 +41,68 @@ public class NumberTest {
     }
 
     @Test
-    public void checkMinimumValueCorrectScenario() {
+    public void checkMinimumValueForCorrectCase() {
     	Number minimum = repository.findFirstByOrderByNumberAsc();
         assertThat(minimum.getNumber() == 100).isFalse();
     }
 
     @Test
-    public void checkMinimumValueWrongScenario() {
+    public void checkMinimumValueForWrongCase() {
     	Number minimum = repository.findFirstByOrderByNumberAsc();
         assertThat(minimum.getNumber() == 100).isFalse();
     }
     
     @Test
-    public void checkMaximumValueCorrectScenario() {
+    public void checkMaximumValueForCorrectCase() {
     	Number minimum = repository.findFirstByOrderByNumberDesc();
         assertThat(minimum.getNumber() == 200).isTrue();
     }
  
 
     @Test
-    public void checkMaximumValueWrongScenario() {
+    public void checkMaximumValueForWrongCase() {
     	Number minimum = repository.findFirstByOrderByNumberDesc();
     	assertThat(minimum.getNumber() == 1).isFalse();
     }
     
     @Test
-    public void checkNumberValueCorrectScenario() {
+    public void checkNumberValueForCorrectCase() {
     	Number singleRecord = repository.findByNumber(11);
     	assertThat(singleRecord).isNotNull();
     }
     
     @Test
-    public void checkNumberValueWrongScenario() {    	
+    public void checkNumberValueForWrongCase() {    	
     	Number singleRecord = repository.findByNumber(1761);
     	assertThat(singleRecord).isNull();
     }
     
+    @Test
+    public void checDescOrderForCorrectCase() {
+    	List<Number> desSortedList = repository.findAll(new Sort(Sort.Direction.DESC, "number"));
+        assertThat(desSortedList.get(0).number == 200).isTrue();
+    }
+    
+    @Test
+    public void checDescOrderForWrongCase() {    	
+    	List<Number> desSortedList = repository.findAll(new Sort(Sort.Direction.DESC, "number"));
+        assertThat(desSortedList.get(0).number == 3).isFalse();
+    }
+    
+    @Test
+    public void checAscOrderForCorrectCase() {
+    	List<Number> desSortedList = repository.findAll(new Sort(Sort.Direction.ASC, "number"));
+        assertThat(desSortedList.get(0).number == 1).isTrue();
+    }
+    
+    @Test
+    public void checAscOrderForWrongCase() {    	
+    	List<Number> desSortedList = repository.findAll(new Sort(Sort.Direction.ASC, "number"));
+        assertThat(desSortedList.get(0).number == 3).isFalse();
+    }
+    
     @After
     public void cleanUp() {
-        
     	repository.deleteAll();
     }
     
